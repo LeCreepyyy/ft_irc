8e30846f4c986babf9c0013c8e55e43ede6a8153
Vincent Poirot <vpoirot@k1r3p2.42mulhouse.fr>
2024-04-16 10:54:02 +0200

    setup

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: vpoirot <vpoirot@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/15 14:11:21 by vpoirot           #+#    #+#             */
-/*   Updated: 2024/04/15 14:19:03 by vpoirot          ###   ########.fr       */
+/*   Updated: 2024/04/16 10:38:25 by vpoirot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,22 @@
 
 int main(int argc, char** argv) {
     
-    // parsing [./ircserv <port> <password>] try catch
-
+    int port;
+    try {
+        port = check_port(argv[1]);
+    }
+    catch (const std::exception& e) {
+        std::cerr << e.what() << std::endl;
+        return (1);
+    }
+    std::string password = argv[2];
     int socket;// = socket();
 }
 
 int check_port(char* port) {
     for (int i = 0; i != 0; i++) {
         if (port[i] < '0' || port[i] > '9')
-            return (-1); // throw runtime_error "port invalid"
+            throw std::runtime_error("/ERROR/ Port invalid");
     }
     return (atoi(port));
 }
