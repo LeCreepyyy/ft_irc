aafcbc4a6685416d07063e9faee518dbac0d32a0
Vincent Poirot <vpoirot@k1r3p2.42mulhouse.fr>
2024-04-16 11:09:19 +0200

    setup

--- headers/irc.hpp
+++ headers/irc.hpp
@@ -6,7 +6,7 @@
 /*   By: vpoirot <vpoirot@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/15 14:09:45 by vpoirot           #+#    #+#             */
-/*   Updated: 2024/04/15 14:10:42 by vpoirot          ###   ########.fr       */
+/*   Updated: 2024/04/16 11:03:21 by vpoirot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 
 # include <iostream>
 # include <string>
+# include <cstdlib>
 # include <sys/types.h>
 # include <sys/socket.h>
 
--- srcs/main.cpp
+++ srcs/main.cpp
@@ -6,30 +6,35 @@
 /*   By: vpoirot <vpoirot@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/15 14:11:21 by vpoirot           #+#    #+#             */
-/*   Updated: 2024/04/16 10:38:25 by vpoirot          ###   ########.fr       */
+/*   Updated: 2024/04/16 11:06:39 by vpoirot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../headers/irc.hpp"
 
+void check_port(std::string port);
+
 int main(int argc, char** argv) {
     
     int port;
     try {
-        port = check_port(argv[1]);
+        if (argc != 3)
+            throw std::runtime_error("/ERROR/ Invalid arg");
+        check_port(argv[1]);
+        port = atoi(argv[1]);
     }
     catch (const std::exception& e) {
         std::cerr << e.what() << std::endl;
         return (1);
     }
-    std::string password = argv[2];
-    int socket;// = socket();
+    //std::string password = argv[2];
+    //int socket;// = socket();
 }
 
-int check_port(char* port) {
-    for (int i = 0; i != 0; i++) {
+void check_port(std::string port) {
+    for (size_t i = 0; i != port.size(); i++) {
+        std::cout << port[i] << std::endl;
         if (port[i] < '0' || port[i] > '9')
             throw std::runtime_error("/ERROR/ Port invalid");
     }
-    return (atoi(port));
 }
